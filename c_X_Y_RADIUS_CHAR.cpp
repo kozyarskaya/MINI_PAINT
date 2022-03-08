fa29943fd376f6b0198649431d50370a623c2ca9
kozyarskaya <92299254+kozyarskaya@users.noreply.github.com>
2022-03-08 14:16:57 +0300

    Update c_X_Y_RADIUS_CHAR.cpp

--- c_X_Y_RADIUS_CHAR.cpp
+++ c_X_Y_RADIUS_CHAR.cpp
@@ -1,6 +1,6 @@
 #include "mini_paint.h"
 
-void c(vector<vector<char>> &background, float x1, float y1, float radius, char ch){
+void c2(vector<vector<char>> &background, float x1, float y1, float radius, char ch){
     x1 = int(x1);
     y1 = int(y1);
     radius = int(radius);
@@ -31,3 +31,21 @@ void c(vector<vector<char>> &background, float x1, float y1, float radius, char 
                 }
             }
         }
+
+
+void c(vector<vector<char>> &background, float x1, float y1, float radius, char ch){
+    x1 = int(x1);
+    y1 = int(y1);
+    radius = int(radius);
+    for(int y = 0; y < background.size(); y++){
+        for (int x = 0; x < background[0].size(); x++){
+           int  point = ((x1 - x) * (x1 - x) + (y1 - y) * (y1 - y));
+           //int  point2 = ((x1 + x) * (x1 + x) + (y1 - y) * (y1 - y));
+           //int  point3 = ((x1 - x) * (x1 - x) + (y1 + y) * (y1 + y));
+           //int  point4 = ((x1 + x) * (x1 + x) + (y1 + y) * (y1 + y));
+            //if (point < (radius * radius) || point2 < (radius * radius) || point3 < (radius * radius)|| point4 < (radius * radius))
+              if (point >= (radius * radius) && point < (radius * radius) + radius)
+                  background[y][x] = ch;
+            }
+    }
+}
